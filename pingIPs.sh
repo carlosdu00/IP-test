6d581ad4753c83e1b3b71529bb437e5098738bd9
carlosdu00 <carloscreed@live.com>
2021-05-21 16:38:28 -0300

    update

--- pingIPs.sh
+++ pingIPs.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-i=0
-
-while [ i -lt 1 ] do
-	date
-	cat /path/to/list.txt |  while read output do
-   		ping -c 1 "$output" > /dev/null
-    		if [ $? -eq 0 ]; then
-    			echo "node $output is up" 
-    		else
-    			echo "node $output is down"
-    		fi
-    		sleep 5
-	done
-done
+IP='192.168.1.100'
+fping -c1 -t300 $IP 2>/dev/null 1>/dev/null
+if [ "$?" = 0 ]
+then
+  echo "Host found"
+else
+  echo "Host not found"
+fi
+sleep 10
