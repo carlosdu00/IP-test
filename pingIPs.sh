c74aceac2317b2ae42ce0b14412e426457387caa
Carlos <carloscreed@live.com>
2021-05-25 17:14:47 -0300

    update

---
+++ pingIPs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+date
+cat list.txt |  while read output
+do
+	ping -n 1 $output > /dev/null
+ 
+	if [ $? -eq 0 ]
+	then
+		echo "$output ON" >> log.txt
+	else
+		echo "$output OFF" >> log.txt
+	fi
+done
