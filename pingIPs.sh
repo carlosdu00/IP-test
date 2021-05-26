014501597130bdf54b26295a03708f9bbfba7e04
carlosdu00 <61929315+carlosdu00@users.noreply.github.com>
2021-05-26 17:19:49 -0300

    Add files via upload

--- pingIPs.sh
+++ pingIPs.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-IP='192.168.1.100'
-fping -c1 -t300 $IP 2>/dev/null 1>/dev/null
-if [ "$?" = 0 ]
-then
-  echo "Host found"
-else
-  echo "Host not found"
-fi
-sleep 10
+
+date
+cat list.txt |  while read output
+do
+	ping -n 1 $output > /dev/null
+	sleep 1
+	if [ $? -eq 0 ]
+	then
+		echo "$output ON" >> log.txt
+	else
+		echo "$output OFF" >> log.txt
+	fi
+done
