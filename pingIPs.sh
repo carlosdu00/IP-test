6e564cc181c2f962683bfec2d6e344c5442111e9
Carlos <carloscreed@live.com>
2021-05-28 22:20:36 -0300

    update

--- pingIPs.sh
+++ pingIPs.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-
-date
-cat list.txt |  while read output
+> log.json
+tamanho=`cat list.txt | wc -l`
+ip=""
+x=1
+nome=""
+echo "\"enderecos\":[" >> log.json
+while [ $x -le $tamanho ]
 do
-	ping -n 1 $output > /dev/null
-	sleep 1
+	ip=`cat list.txt | tail -n$x| head -n 1`
+	nome=`cat list.txt | tail -n$(($x+1))| head -n 1`
+	ping -n 1 $ip > /dev/null
 	if [ $? -eq 0 ]
 	then
-		echo "$output ON" >> log.txt
+		echo "{\"nome\":\"$nome\", \"ip\":\"$ip\", \"status\":\"on\"}," >> log.json
 	else
-		echo "$output OFF" >> log.txt
+		echo "{\"nome\":\"$nome\", \"ip\":\"$ip\", \"status\":\"off\"}," >> log.json
 	fi
+	x=$((x+2))
 done
+echo "]" >> log.json
